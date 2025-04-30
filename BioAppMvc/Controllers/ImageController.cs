06e6218363961031e5358746d4711ed0bcf43401
Stefanos Niktaris <stefanosniktaris@192.168.2.6>
2025-04-30 21:52:25 +0300

    test 7?

--- BioAppMvc/Controllers/ImageController.cs
+++ BioAppMvc/Controllers/ImageController.cs
@@ -1,33 +1,57 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using BlobService.Services; // Adjust this to where BlobService is
+using System.IO;
 
-public class ImageController : Controller
+namespace BioAppMvc.Controllers
 {
-    private readonly BlobService _blobService;
-
-    public ImageController(BlobService blobService)
+    public class ImageController : Controller
     {
-        _blobService = blobService;
-    }
+        private readonly BlobService _blobService;
 
-    // Upload file
-    [HttpPost]
-    public async Task<IActionResult> Upload(IFormFile file)
-    {
-        if (file != null)
+        public ImageController(BlobService blobService)
         {
-            var fileUrl = await _blobService.UploadFileAsync(file);
-            ViewBag.FileUrl = fileUrl; // Display URL to the user
+            _blobService = blobService;
         }
-        return View();
-    }
 
-    // Get file URL (for API)
-    [HttpGet]
-    public IActionResult Get(string fileName)
-    {
-        var fileUrl = _blobService.GetBlobUrl(fileName);
-        return Redirect(fileUrl);
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var files = await _blobService.ListFilesAsync();
+            return View(files);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            if (file != null && file.Length > 0)
+            {
+                using var stream = file.OpenReadStream();
+                var fileUrl = await _blobService.UploadFileAsync(stream, file.FileName, file.ContentType);
+                ViewBag.Message = "File uploaded successfully: " + fileUrl;
+            }
+            else
+            {
+                ViewBag.Message = "Please select a valid file.";
+            }
+
+            var files = await _blobService.ListFilesAsync();
+            return View("Index", files);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string fileName)
+        {
+            var result = await _blobService.DeleteFileAsync(fileName);
+
+            ViewBag.Message = result
+                ? $"File '{fileName}' deleted successfully."
+                : $"File '{fileName}' not found or already deleted.";
+
+            var files = await _blobService.ListFilesAsync();
+            return View("Index", files);
+        }
     }
 }
--- BioAppMvc/Services/BlobService.cs
+++ BioAppMvc/Services/BlobService.cs
@@ -1,52 +1,59 @@
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
 
-public class BlobService
+namespace BioAppMvc.Services
 {
-    private readonly BlobContainerClient _containerClient;
-
-    public BlobService(IConfiguration configuration)
+    public class BlobService
     {
-        var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
-        var containerName = configuration["AzureStorage:ContainerName"];
+        private readonly BlobContainerClient _containerClient;
 
-        _containerClient = new BlobContainerClient(connectionString, containerName);
-        _containerClient.CreateIfNotExists(PublicAccessType.Blob);
-    }
-
-    public async Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType)
-    {
-        var blobClient = _containerClient.GetBlobClient(fileName);
-        await blobClient.UploadAsync(fileStream, overwrite: true);
-        await blobClient.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = contentType });
-
-        return blobClient.Uri.ToString();
-    }
-
-    public async Task<Stream> DownloadFileAsync(string fileName)
-    {
-        var blobClient = _containerClient.GetBlobClient(fileName);
-        var downloadInfo = await blobClient.DownloadAsync();
-        return downloadInfo.Value.Content;
-    }
-
-    public async Task<bool> DeleteFileAsync(string fileName)
-    {
-        var blobClient = _containerClient.GetBlobClient(fileName);
-        return await blobClient.DeleteIfExistsAsync();
-    }
-
-    public async Task<List<string>> ListFilesAsync()
-    {
-        var fileList = new List<string>();
-
-        await foreach (BlobItem blobItem in _containerClient.GetBlobsAsync())
+        public BlobService(IConfiguration configuration)
         {
-            var blobClient = _containerClient.GetBlobClient(blobItem.Name);
-            fileList.Add(blobClient.Uri.ToString());
+            var connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTION_STRING");
+            var containerName = configuration["AzureStorage:ContainerName"];
+
+            _containerClient = new BlobContainerClient(connectionString, containerName);
+            _containerClient.CreateIfNotExists(PublicAccessType.Blob);
         }
 
-        return fileList;
+        public async Task<string> UploadFileAsync(Stream fileStream, string fileName, string contentType)
+        {
+            var blobClient = _containerClient.GetBlobClient(fileName);
+            await blobClient.UploadAsync(fileStream, overwrite: true);
+            await blobClient.SetHttpHeadersAsync(new BlobHttpHeaders { ContentType = contentType });
+
+            return blobClient.Uri.ToString();
+        }
+
+        public async Task<Stream> DownloadFileAsync(string fileName)
+        {
+            var blobClient = _containerClient.GetBlobClient(fileName);
+            var downloadInfo = await blobClient.DownloadAsync();
+            return downloadInfo.Value.Content;
+        }
+
+        public async Task<bool> DeleteFileAsync(string fileName)
+        {
+            var blobClient = _containerClient.GetBlobClient(fileName);
+            return await blobClient.DeleteIfExistsAsync();
+        }
+
+        public async Task<List<string>> ListFilesAsync()
+        {
+            var fileList = new List<string>();
+
+            await foreach (BlobItem blobItem in _containerClient.GetBlobsAsync())
+            {
+                var blobClient = _containerClient.GetBlobClient(blobItem.Name);
+                fileList.Add(blobClient.Uri.ToString());
+            }
+
+            return fileList;
+        }
     }
 }
